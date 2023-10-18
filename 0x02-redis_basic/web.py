7846fce6f792f25f7eeac6a0d195aa7b23d7b811
williamsHack <williamyawdickson1998@gmail.com>
2023-10-18 17:20:49 +0000

    By his Grace

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python3
+""" expiring web cache module """
+
+import redis
 import requests
-import time
+from typing import Callable
 from functools import wraps
-from typing import Dict
 
-cache: Dict[str, str] = {}
+redis = redis.Redis()
 
-def get_page(url: str) -> str:
-    if url in cache:
-        print(f"Retrieving from cache: {url}")
-        return cache[url]
-    else:
-        print(f"Retrieving from web: {url}")
-        response = requests.get(url)
-        result = response.text
-        cache[url] = result
+
+def wrap_requests(fn: Callable) -> Callable:
+    """ Decorator wrapper """
+
+    @wraps(fn)
+    def wrapper(url):
+        """ Wrapper for decorator guy """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
         return result
 
-def cache_with_expiration(expiration: int):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            url = args[0]
-            key = f"count:{url}"
-            if key in cache:
-                count, timestamp = cache[key]
-                if time.time() - timestamp > expiration:
-                    result = func(*args, **kwargs)
-                    cache[key] = (count+1, time.time())
-                    return result
-                else:
-                    cache[key] = (count+1, timestamp)
-                    return
+    return wrapper
+
+
+@wrap_requests
+def get_page(url: str) -> str:
+    """get page self descriptive
+    """
+    response = requests.get(url)
+    return response.text
