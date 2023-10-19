a2251777c532fbd77b1f2f2f60fe93201aea79f5
williamsHack <williamyawdickson1998@gmail.com>
2023-10-19 09:17:45 +0000

    By His Grace

--- 0x02-redis_basic/0x00-MySQL_Advanced/0-uniq_users.sql
+++
@@ -1,7 +0,0 @@
--- A SQL script that creates a table (called users) with following fields
--- id, email, name
-CREATE TABLE IF NOT EXISTS users (
-	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255)
-)
