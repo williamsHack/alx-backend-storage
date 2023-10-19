85d6192aee93c6f16f925e12fb8d3212677bc9d9
williamsHack <williamyawdickson1998@gmail.com>
2023-10-19 10:44:14 +0000

    By His Grace

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+#""" Main file """
+
+#import redis
+
+#Cache = __import__('exercise').Cache
+
+#cache = Cache()
+
+#data = b"hello"
+#key = cache.store(data)
+#print(key)
+
+#local_redis = redis.Redis()
+#print(local_redis.get(key))
+
 """ Main file """
 
 Cache = __import__('exercise').Cache
@@ -18,16 +33,13 @@ outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, 
 print("inputs: {}".format(inputs))
 print("outputs: {}".format(outputs))
 
-# #!/usr/bin/env python3
-# """ Main file """
+Cache = __import__('exercise').Cache
 
-# Cache = __import__('exercise').Cache
+cache = Cache()
 
-# cache = Cache()
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
 
-# cache.store(b"first")
-# print(cache.get(cache.store.__qualname__))
-
-# cache.store(b"second")
-# cache.store(b"third")
-# print(cache.get(cache.store.__qualname__))
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
