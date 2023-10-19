e4a7c14b9919ccb9a0385a9360d2bc85744b4b29
williamsHack <williamyawdickson1998@gmail.com>
2023-10-19 09:59:37 +0000

    By His Grace

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """
-Caching request module for a different URL
+Caching request module for a different URL with 30-second cache expiration
 """
 import redis
 import requests
 from functools import wraps
 from typing import Callable
-
 
 def track_get_page(fn: Callable) -> Callable:
     """ Decorator for get_page
@@ -23,26 +22,25 @@ def track_get_page(fn: Callable) -> Callable:
         if cached_page:
             return cached_page.decode('utf-8')
         response = fn(url)
-        client.set(f'{url}', response, 10)
+        client.set(f'{url}', response, 30)  # Cache with a 30-second expiration
         return response
-    return wrapper
 
+    return wrapper
 
 @track_get_page
 def get_page(url: str) -> str:
-    """ Makes an HTTP request to a different endpoint
+    """ Makes an HTTP request to a given endpoint
     """
     response = requests.get(url)
     return response.text
 
 if __name__ == "__main__":
-    # Example usage of the decorated get_page function
-    url = "http://slowwly.robertomurray.co.uk"  # Replace with the desired URL
-    content = get_page(url)
-    print(content)
+    # Test the get_page function with a different URL
+    for _ in range(5):
+        content = get_page("http://slowwly.robertomurray.co.uk")  # Replace with your desired URL
+        print(content)
 
     # Get the access count for the URL
-    client = redis.Redis()
-    access_count = client.get(f'count:{url}')
-    print(f"Access count for {url}: {access_count.decode('utf-8')}")
+    access_count = redis.Redis().get("count:http://slowwly.robertomurray.co.uk")  # Replace with your desired URL
+    print(f"Access count: {access_count.decode('utf-8')}")
 
